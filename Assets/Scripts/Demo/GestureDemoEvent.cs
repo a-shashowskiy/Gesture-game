001921aa475525ee674ff41ac10e416bacf3656a
Александр Шаховский <assassin_47@ukr.net>
2016-04-11 17:46:16 +0300

    Final Update

--- Assets/Scripts/Demo/GestureDemoEvent.cs
+++ Assets/Scripts/Demo/GestureDemoEvent.cs
@@ -3,10 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using GestureRecognizer;
 using UnityEngine.UI;
-//using Gesture;
-
-// !!!: Drag & drop a GestureRecognizer prefab on to the scene first from Prefabs folder!!!
-//using UnityEditor;
 using System.Globalization;
 using System.Runtime.Remoting.Lifetime;
 using System.Security.Policy;
@@ -39,7 +35,7 @@ public class GestureDemoEvent : MonoBehaviour {
 
 	void Start() 
 	{     
-		//cube.Name = "rectangle";
+		
 		gl = new GestureLibrary ("gestures");
 		Life = new GameObject[3];
 		figGesture = new GameObject[5];
@@ -66,7 +62,9 @@ public class GestureDemoEvent : MonoBehaviour {
 		string recGestTemp = "none";
 		float tempScore = 0.0f;
 
-		TimerDown ();
+		TimerDown ();// Start timer when press Start button
+		LifeShow ();// Show remaine life 
+
 			if (Input.GetMouseButtonDown(0)) {     
 				points.Clear(); 
 
@@ -74,6 +72,7 @@ public class GestureDemoEvent : MonoBehaviour {
 			if (Input.GetMouseButton(0)) {    
 				points.Add(new Vector2(Input.mousePosition.x, -Input.mousePosition.y)); 
 			}
+		// Recognize Gesture inputed in to Vector2 points
 			if (Input.GetMouseButtonUp(0))
 			{
 				Gesture g = new Gesture(points);
@@ -83,55 +82,18 @@ public class GestureDemoEvent : MonoBehaviour {
 				SetMessage("Gesture is recognized as <color=#ff0000>'" + r.Name + "'</color> with a accuracy of " + r.Score);
 				if (life != 0 || lose == false) 
 				{
-					LifeShow ();
+					// Count score from recognized gusture 
 					ScoreCount (tempScore);
 				}
-			}else if (life == 0 || lose == true) 
-			{
-				loseText.text = "Game over";
-				RestBtn.gameObject.SetActive(lose);
-			}
-/*		while (stage < 3) 
-		{	
-			switch (stage) 
-			{
-			case 0:
-				if (figGesture [0].activeInHierarchy == false) {
-					figGesture [0].gameObject.SetActive (true);
-					StartCoroutine (LateCall (figGesture [0]));
-					if (recGestTemp != "infinity") {
-						life = life - 1;
-					}else stage++;
-				}
-				
-				break;
-			case 1:
-				if (figGesture [1].activeInHierarchy == false) {
-					figGesture [1].gameObject.SetActive (true);
-					StartCoroutine (LateCall (figGesture [1]));
-					if (recGestTemp!= "rectangle") {
-						life = life - 1;
-					} else stage++;
-				}
-				
-				break;
-			case 2:
-				if (figGesture [2].activeInHierarchy == false) {
-					figGesture [2].gameObject.SetActive (true);
-					StartCoroutine (LateCall (figGesture [2]));
-					if (recGestTemp != "circle") {
-						life = life - 1;
-					} else stage++;
-				}
-				break;
-			default:
-				break;
-			}
-		}
-*/
+			}else if (lose == true) 
+					{
+						loseText.text = "Game over";
+						RestBtn.gameObject.SetActive(lose);
+					}
 
 	}
 
+	//Life 
 	void LifeShow(){
 		switch(life){
 		case 2:
@@ -151,79 +113,12 @@ public class GestureDemoEvent : MonoBehaviour {
 			break;
 		}
 	}
-   /*
-    // Subscribe your own method to OnRecognition event 
-    void OnEnable() {
-        GestureBehaviour.OnRecognition += OnGestureRecognition;
-    }
 
-    // Unsubscribe when this game object or monobehaviour is disabled.
-    // If you don't unsubscribe, this will give an error.
-    void OnDisable() {
-        GestureBehaviour.OnRecognition -= OnGestureRecognition;
-    }
-
-    // Unsubscribe when this game object or monobehaviour is destroyed.
-    // If you don't unsubscribe, this will give an error.
-    void OnDestroy() {
-        GestureBehaviour.OnRecognition -= OnGestureRecognition;
-    }
-
-
-    /// <summary>
-    /// The method to be called on recognition event
-    /// </summary>
-    /// <param name="r">Recognition result</param>
-    /// 
-    /// <remarks>
-    /// Implement your own method here. This method will be called by GestureBehaviour
-    /// automatically when a gesture is recognized. You can write your own script
-    /// in this method (kill enemies, shoot a fireball, or cast some spell etc.)
-    /// This method's signature should match the signature of OnRecognition event,
-    /// so your method should always have one parametre with the type of Result. Example:
-    /// 
-    /// void MyMethod(Result gestureResult) {
-    ///     kill enemy,
-    ///     shoot fireball,
-    ///     cast spell etc.
-    /// }
-    /// 
-    /// You can decide what to do depending on the name of the gesture and its score.
-    /// For example, let's say, if the player draws the letter of "e" (let's name the 
-    /// gesture "Fireball") and it is 50% similar to our "Fireball" gesture, shoot a fireball:
-    /// 
-    /// void MagicHandler(Result magicGesture) {
-    /// 
-    ///    if (magicGesture.Name == "Fireball" && magicGesture.Score >= 0.5f) {
-    ///        Instantiate(fireball, transform.position, Quaternion.identity);
-    ///    }
-    /// 
-    /// }
-    /// 
-    /// !: You can name this method whatever you want, but you should use the same name
-    /// when subscribing and unsubscribing. If your method's name is MagicHandler like above,
-    /// then:
-    /// 
-    /// void OnEnable() {
-    ///   GestureBehaviour.OnRecognition += MagicHandler;
-    /// }
-    /// </remarks>
-    void OnGestureRecognition(Result r) {
-       SetMessage("Gesture is recognized as <color=#ff0000>'" + r.Name + "'</color> with a score of " + r.Score);
-	
-	}
-	*/
+	//Restart method on button "Restart"
 	public void Restart(){
 		SceneManager.LoadScene(scene.name);
 	}
-	/*
-	void OnGUI() {
-		if (lose == true) {
-			if (GUI.Button (new Rect (500, 30, 400, 30), "Restart")) {
-				Restart ();
-			} 
-		}
-	}*/
+
     /// <summary>
     /// Shows a message at the bottom of the screen
     /// </summary>
@@ -232,6 +127,8 @@ public class GestureDemoEvent : MonoBehaviour {
     public void SetMessage(string text) {
         messageArea.text = text;
     }
+
+	//Timer function
 	void TimerDown(){
 		if (timer > 0.0f && lose != true) {
 			timer -= Time.smoothDeltaTime;
@@ -242,6 +139,7 @@ public class GestureDemoEvent : MonoBehaviour {
 		}
 	}
 
+	// Funciton for calculate score
 	void ScoreCount(float res){
 			if (res >= 0.9f)
 			{
@@ -258,13 +156,4 @@ public class GestureDemoEvent : MonoBehaviour {
 				life = life - 1;
 			}
 		}
-
-	IEnumerator LateCall(GameObject gmobg)
-	{
-
-		yield return new WaitForSeconds(1.5f);
-
-		gmobg.SetActive(false);
-
-	}
 }
